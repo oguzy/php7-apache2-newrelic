ddce9f23356b355d812bf514768dd8fea50d2464
Oğuz Yarımtepe <oyarimtepe@ebay.com>
2017-04-10 14:42:49 +0300

    run the command at foreground

--- start_for_docker.sh
+++ start_for_docker.sh
@@ -2,6 +2,6 @@
 
 sed -i -e 's/newrelic.appname = "PHP Application"/newrelic.appname = \"'"$APP_NAME"'\"/' /etc/php7/conf.d/newrelic.ini
 
-/usr/bin/newrelic-daemon -c /etc/newrelic/newrelic.cfg --pidfile /var/run/newrelic-daemon.pid
+/usr/bin/newrelic-daemon -c /etc/newrelic/newrelic.cfg --pidfile /var/run/newrelic-daemon.pid -f
 
 /usr/local/bin/httpd-foreground
