12c9e36af3f63532359be98d69c27a705324a77c
Oğuz Yarımtepe <oyarimtepe@ebay.com>
2017-06-12 20:43:38 +0300

    files to build test image

--- newrelic-install.sh
+++ newrelic-install.sh
@@ -11,7 +11,7 @@ cd newrelic-php5-7.2.0.191-linux-musl
 
 sh newrelic-install install << EOF
 y
-1395c36ef8f513749aa05c18e50abf1172b351b9
+our_secret_key
 EOF
 
 #cp /etc/newrelic/newrelic.cfg.template /etc/newrelic/newrelic.cfg
--- start_for_docker.sh
+++ start_for_docker.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
+mv /etc/php7/conf.d/newrelic.ini.dist /etc/php7/conf.d/newrelic.ini
+
 sed -i -e 's/newrelic.appname = "PHP Application"/newrelic.appname = \"'"$APP_NAME"'\"/' /etc/php7/conf.d/newrelic.ini
 
-#echo 'newrelic.daemon.port = "@newrelic-daemon"' >> /etc/php7/conf.d/newrelic.ini
-
-#echo 'newrelic.daemon.utilization.detect_docker = true' >> /etc/php7/conf.d/newrelic.ini
-
-#/usr/bin/newrelic-daemon -c /etc/newrelic/newrelic.cfg --pidfile /var/run/newrelic-daemon.pid -f
-
-echo 'newrelic.daemon.utilization.detect_aws = false' >> /etc/php7/conf.d/newrelic.ini
-echo 'newrelic.transaction_tracer.slow_sql = false' >> /etc/php7/conf.d/newrelic.ini
+echo "newrelic.transaction_tracer.slow_sql = false" >> /etc/php7/conf.d/newrelic.ini
+echo "newrelic.daemon.utilization.detect_aws = false" >> /etc/php7/conf.d/newrelic.ini
+echo "newrelic.daemon.utilization.detect_docker = true" >> /etc/php7/conf.d/newrelic.ini
+echo "newrelic.enabled = true" >> /etc/php7/conf.d/newrelic.ini
 
 /usr/local/bin/httpd-foreground
